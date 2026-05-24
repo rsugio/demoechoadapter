f9c24a095831c3c34cbce6c7d0077bc74552dee8
Илья Кузнецов <chumpa@yandex.ru>
2026-05-24 09:01:49 +0300

    мелочи

--- src/main/java/demoecho/ODataAdapterException.java
+++ src/main/java/demoecho/ODataAdapterException.java
@@ -1,31 +1,31 @@
 package demoecho;
 
 public class ODataAdapterException extends Exception {
-    private static final String ERROR_CODE_PREFIX = "ODEX_";
+    private static final String ERROR_CODE_PREFIX = "ECHO_";
     private static final long serialVersionUID = 1L;
     private ODataAdapterError errorCode;
     private String errorMessage;
 
     public ODataAdapterException(ODataAdapterError errorCode) {
         this.errorCode = errorCode;
-        this.errorMessage = errorCode + ":" + com.sap.aii.adapter.odata.ra.xi.util.Util.localizeMessage("ODEX_" + errorCode.toString());
+        this.errorMessage = errorCode + ":" + Util.localizeMessage(ERROR_CODE_PREFIX + errorCode.toString());
     }
 
     public ODataAdapterException(ODataAdapterError errorCode, String... params) {
         this.errorCode = errorCode;
-        this.errorMessage = errorCode + ":" + com.sap.aii.adapter.odata.ra.xi.util.Util.localizeMessage("ODEX_" + errorCode.toString(), params);
+        this.errorMessage = errorCode + ":" + Util.localizeMessage(ERROR_CODE_PREFIX + errorCode.toString(), params);
     }
 
     public ODataAdapterException(Exception ex, ODataAdapterError errorCode) {
         super(ex);
         this.errorCode = errorCode;
-        this.errorMessage = errorCode + ":" + com.sap.aii.adapter.odata.ra.xi.util.Util.localizeMessage("ODEX_" + errorCode.toString());
+        this.errorMessage = errorCode + ":" + Util.localizeMessage(ERROR_CODE_PREFIX + errorCode.toString());
     }
 
     public ODataAdapterException(Exception ex, ODataAdapterError errorCode, String... params) {
         super(ex);
         this.errorCode = errorCode;
-        this.errorMessage = errorCode + ":" + Util.localizeMessage("ODEX_" + errorCode.toString(), params);
+        this.errorMessage = errorCode + ":" + Util.localizeMessage(ERROR_CODE_PREFIX + errorCode.toString(), params);
     }
 
     public ODataAdapterError getError() {
